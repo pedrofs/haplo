9f93dc612c26c5d93a7cfdefa2e721514e0e2afa
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-10-22 17:03:21 -0200

    new schema with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926164911) do
+ActiveRecord::Schema.define(version: 20141021214531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,9 +57,13 @@ ActiveRecord::Schema.define(version: 20140926164911) do
     t.string   "name"
     t.text     "description"
     t.string   "client"
-    t.boolean  "archived",    default: false, null: false
+    t.boolean  "archived",           default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "tasks", force: true do |t|
