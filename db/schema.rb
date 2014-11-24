2475375ebdf72e43fba45b79d7f7a8f5ea065ad8
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-24 15:40:47 -0200

    adding image fields to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122192415) do
+ActiveRecord::Schema.define(version: 20141124141225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,10 @@ ActiveRecord::Schema.define(version: 20141122192415) do
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
     t.integer  "role_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
