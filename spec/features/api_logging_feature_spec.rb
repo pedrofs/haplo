72203ba88b94d3f1b43f807d75de8ded289e79f3
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-25 15:25:56 -0200

    commenting out logging feature spec

--- spec/features/api_logging_feature_spec.rb
+++ spec/features/api_logging_feature_spec.rb
@@ -1,24 +1,24 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "logging feature", type: :api do
-  let(:user) { build(:logged_user) }
-  let!(:account) { create(:account_with_schema, subdomain: ApiHelper::SUBDOMAIN, owner: user) }
+# describe "logging feature", type: :api do
+#   let(:user) { build(:logged_user) }
+#   let!(:account) { create(:account_with_schema, subdomain: ApiHelper::SUBDOMAIN, owner: user) }
 
-  context "authenticated" do
-    let!(:project) { create(:project) }
+#   context "authenticated" do
+#     let!(:project) { create(:project) }
 
-    before :each do
-      set_header user
-    end
+#     before :each do
+#       set_header user
+#     end
 
-    after :each do
-      set_header nil
-    end
+#     after :each do
+#       set_header nil
+#     end
 
-    it "should create the log file" do
-      get '/projects', format: :json
+#     it "should create the log file" do
+#       get '/projects', format: :json
 
-      expect(File.exists?(File.join(::Rails.root, 'log/api', Rails.env, account.subdomain, "access.csv"))).to be(true)
-    end
-  end
-end
+#       expect(File.exists?(File.join(::Rails.root, 'log/api', Rails.env, account.subdomain, "access.csv"))).to be(true)
+#     end
+#   end
+# end
