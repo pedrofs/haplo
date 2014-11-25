59b0c4cece2dea018f096d6c5133970074197997
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-25 15:25:41 -0200

    adding task fixture

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
   factory :task do
     title 'Task Title'
     description 'task description'
+
+    factory :task_with_users_and_project do
+      association :assigned, factory: :user, strategy: :build
+      association :reporter, factory: :user, strategy: :build
+      association :taskable, factory: :project, strategy: :build
+    end
   end
 end
