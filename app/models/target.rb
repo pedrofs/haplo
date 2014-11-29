4d4aa423236bea821e37a389e1f41aee297d8066
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-29 16:32:19 -0200

    adding after_remove callback to target in order to remove discussion

--- app/models/target.rb
+++ app/models/target.rb
@@ -4,4 +4,12 @@ class Target < ActiveRecord::Base
 
   validates :discussion, presence: true
   validates :targetable, presence: true
+
+  after_destroy :remove_discussion, if: -> { discussion.targets.count == 0 }
+
+  private
+
+  def remove_discussion
+    discussion.destroy!
+  end
 end
