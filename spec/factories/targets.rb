336a6a37e236e4bca319dc5ce578c8b89c95e343
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-29 16:33:49 -0200

    adding factories for target

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -1,6 +1,15 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :targets do
+  factory :target do
+    association :discussion, factory: :discussion, strategy: :build
+
+    factory :project_target do
+      association :targetable, factory: :project, strategy: :build
+    end
+
+    factory :task_target do
+      association :targetable, factory: :task, strategy: :build
+    end
   end
 end
