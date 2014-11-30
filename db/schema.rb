61562a048232da3c4b09fe1a730f50f7c3813de5
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-11-30 16:10:39 -0200

    adding title to discussions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129205928) do
+ActiveRecord::Schema.define(version: 20141130172215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20141129205928) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   add_index "discussions", ["user_id"], name: "index_discussions_on_user_id", using: :btree
