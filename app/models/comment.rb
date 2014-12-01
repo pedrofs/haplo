15af1a94cfc71aa1d7cff4e4936ed12942eed015
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-12-01 15:58:23 -0200

    adding comment to_builder

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,23 @@ class Comment < ActiveRecord::Base
 
   validates :user, presence: true
   validates :content, presence: true
+  validates :discussion, presence: true
+
+  def to_builder
+    Jbuilder.new do |comment|
+      comment.id id
+      comment.content content
+      comment.created_at created_at
+      comment.updated_at updated_at
+      comment.user do
+        comment.id user.id
+        comment.name user.name
+        comment.image do
+          comment.small user.image :small
+          comment.medium user.image :medium
+          comment.big user.image :big
+        end
+      end
+    end
+  end
 end
