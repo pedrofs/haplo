7fa69c1f5de93a7ba16d870aeed4b1ba1a9cfb5f
Pedro Fernandes Steimbruch <pedrofsteimbruch@gmail.com>
2014-12-02 14:18:35 -0200

    adding graphs to project overview

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -14,10 +14,14 @@
 //= require vendor/moment.min
 //= require vendor/moment.locale.pt-br
 //= require vendor/angular-moment.min
+
 //= require vendor/jquery.jqplot.min
 //= require vendor/jqplot.pieRenderer
 //= require vendor/jqplot.donutRenderer
+//= require vendor/jqplot.canvasAxisLabelRenderer
+//= require vendor/jqplot.canvasTextRenderer
 //= require vendor/ui-chart.min
+
 //= require vendor/daterangepicker.min
 //= require vendor/angular-daterangepicker.min
 //= require vendor/textAngular
